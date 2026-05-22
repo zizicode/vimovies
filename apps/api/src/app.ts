a3e962f778e3ada90d1f132663b6df4c54f1771b
zizicode <victordoblezz@gmail.com>
2026-05-22 00:45:15 -0400

    Fix Cors apiii

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -3,17 +3,16 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { logger } from 'hono/logger'
 import { prettyJSON } from 'hono/pretty-json'
-import { secureHeaders } from 'hono/secure-headers'
 import { env } from '@vimovies/db'
 import routes from './routes/index.js'
 import { sitemapRoutes } from './routes/sitemap.routes.js'
 
 export const app = new Hono()
   .use('*', logger())
-  .use('*', secureHeaders())
+
   .use('*', cors({
     origin: [env.WEB_URL, env.DASHBOARD_URL, 'https://www.vimovies.com', 'https://vimovies.com'],
-    allowMethods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowMethods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   }))
