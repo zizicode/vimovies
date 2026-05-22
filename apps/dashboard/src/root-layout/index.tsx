0e4ea25ea8dd016f87ad250ce84f38b5a2b83153
zizicode <victordoblezz@gmail.com>
2026-05-22 00:09:33 -0400

    Fix TypeScript build errors for dashboardlllkkhg;djsddf

--- apps/dashboard/src/root-layout/index.tsx
+++ apps/dashboard/src/root-layout/index.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom'
-import { Sidebar } from '../components/sidebar'
+import { Sidebar } from '../components/ui/sidebar'
 
 export default function RootLayout() {
   return (
